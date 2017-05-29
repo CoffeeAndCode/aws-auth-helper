988f99993a0ef9936171d910d1870f093723cb9f
Jonathan Knapp <jon@coffeeandcode.com>
2017-05-29 09:30:45 -0400

    Refactor logic and add tests around different parts of the app

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const getCredentialsForBash = require('./lib/getCredentialsForBash');
+
+getCredentialsForBash({
+  argv: process.argv.slice(2),
+  env: process.env,
+  stderr: process.stderr,
+  stdin: process.stdin,
+  stdout: process.stdout,
+}).catch((error) => {
+  process.stderr.write(error.message + "\n");
+  process.exitCode = 1;
+});
